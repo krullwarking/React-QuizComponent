276bdaddf14ae00ef4eec8cd73f08bd7c6e85d67
krullwarking <krullwarking@yahoo.com>
2019-03-06 11:05:05 -0600

    2
    2

--- src/Quiz.js
+++ src/Quiz.js
@@ -9,7 +9,7 @@ class Quiz extends Component {
         }
     render() {
         return(<div>
-            <div classname="QuizQuestion">
+            <div className="QuizQuestion">
                 {quizData.quiz_questions[0].instruction_text}
             </div>
         </div>)
